886b54e781c7d786426d214bb35f5f1731fbb87a
Reeyou <ryu993830137@163.com>
2019-11-20 16:38:32 +0800

    uniapp created

---
+++ pages/Home/cmps/pageHeader.vue
@@ -0,0 +1,82 @@
+<style lang='scss'>
+	.header {
+		width: 100%;
+		box-sizing: border-box;
+		padding: 20upx;
+		display: flex;
+		align-items: center;
+		.location {
+			width: 120upx;
+			display: flex;
+			align-item: center;
+			> i {
+				font-size: 40upx;
+				color: #ffc50a;
+			}
+			.city {
+				font-size: 26upx;
+				/* font-weight: bold; */
+				padding-left: 4upx;
+				display: flex;
+				align-items: center;
+				color: #010101;
+			}
+		}
+		.search {
+			flex: 1;
+			height: 60upx;
+			border: 1upx solid #ddd;
+			border-radius: 30upx;
+			position: relative;
+			display: flex;
+			align-items: center;
+			> input {
+				padding: 0 28upx;
+				font-size: 24upx;
+				height: 24upx;
+			}
+			> i {
+				position: absolute;
+				top: 8upx;
+				right: 28upx;
+				font-size: 36upx;
+				color: #aaa;
+			}
+		}
+			
+		.msg {
+			width: 40px;
+			text-align: center;
+			> i {
+				font-size: 40upx;
+			}
+		}
+	}
+</style>
+<template>
+	<view class='header'>
+		<view class='location'>
+			<i class="iconfont icon-location"></i>
+			<view class="city">{{city}}</view>
+		</view>
+		<view class="search">
+			<input type="text" placeholder="请输入搜索内容..." placeholder-style="color:#c0c0c0">
+			<i class='iconfont icon-search'></i>
+		</view>
+		<view class="msg">
+			<i class='iconfont icon-mark'></i>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				city: '北京'
+			}
+		}
+	}
+</script>
+
+
---
+++ pages/Home/home.vue
@@ -0,0 +1,15 @@
+<template>
+	<page-header></page-header>
+</template>
+
+<script>
+	import pageHeader from './cmps/pageHeader'
+	export default {
+		components: {
+			pageHeader
+		}
+	}
+</script>
+
+<style>
+</style>
